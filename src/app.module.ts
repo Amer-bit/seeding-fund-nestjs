564836f9e2fd3b157d5242aca3f085f35d5b4ae6
amer-bit <ny.1995@outlook.com>
2021-09-06 20:59:29 +0300

    setup config module and database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI')
+      }),
+      inject: [ConfigService]
+    }),
+    ConfigModule.forRoot({
+      envFilePath: ['.env', '.env-prodiction'],
+      isGlobal: true,
+    }),
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
