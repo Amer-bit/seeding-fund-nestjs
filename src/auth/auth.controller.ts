f1e446cc80345ab4276790d4c6261480b16dd444
amer-bit <ny.1995@outlook.com>
2021-09-06 17:17:12 +0300

    created register route for project owner and login route for both project owner and admin

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,26 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 import { ProjectOwnerRegiseterDto } from './dto/project-owner.register.dto';
 
 @Controller()
 export class AuthController {
 
+    constructor(
+        private authService: AuthService,
+    ){}
+
     @Post('regiester')
     async register(registerDto: ProjectOwnerRegiseterDto){
+        return this.authService.register(registerDto);
+    }
+
+    @Get('login')
+    async logIn(){
         
+    }
+
+    @Get('/admin/login')
+    async adminLogIn(){
+
     }
 }
