0b59b458a1e447f76fa71250f66bf59aa9a6915e
amer-bit <ny.1995@outlook.com>
2021-09-07 00:17:57 +0300

    renamed authrepo to userrepo

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,24 +1,24 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { AuthRepository } from './auth.repository';
+import { UserRepository } from './user.repository';
 import { LoginDto } from './dto/login.dto';
 import { ProjectOwnerRegiseterDto } from './dto/project-owner.register.dto';
 
 @Injectable()
 export class AuthService {
     constructor(
-        private authRepo: AuthRepository,
+        private userRepo: UserRepository,
         private jwtService: JwtService,
     ){}
 
     async register(registerDto: ProjectOwnerRegiseterDto){
-        return this.authRepo.register(registerDto);
+        return this.userRepo.register(registerDto);
     }
 
     // Verify user password and email and return 
     // accesstoken to be used for later requests
     async login(loginDto: LoginDto): Promise<{accessToken: string}>{
-        const { email } = await this.authRepo.login(loginDto);
+        const { email } = await this.userRepo.login(loginDto);
         if(!email) throw new UnauthorizedException("The email or password provided is invalid");
         const accessToken = await this.jwtService.signAsync({email});
         return {accessToken}; 
