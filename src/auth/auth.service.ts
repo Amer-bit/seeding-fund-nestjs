ffce4d762d9d8bee0335c0bd715b4615bb823a9c
amer-bit <ny.1995@outlook.com>
2021-09-06 17:51:15 +0300

    created register service but without database layer

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { ProjectOwnerRegiseterDto } from './dto/project-owner.register.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(){}
+    private database = [];
+    async register(registerDto: ProjectOwnerRegiseterDto){
+        const { username, email, password } = registerDto;
+        let hashedPassword = await this.hashPassword(password);
+        let userObj = {
+            username,
+            email,
+            password: hashedPassword
+        }
+
+        this.database.push(userObj);
+        return userObj;
+    }
+
+
+    private async hashPassword(password: string): Promise<string>{
+        let salt = await bcrypt.genSalt();
+        let hashedPassword = await bcrypt.hash(password, salt);
+        return hashedPassword;
+    }
+}
