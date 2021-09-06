1471907d00e41c308ebd52e8175107f5ace993de
amer-bit <ny.1995@outlook.com>
2021-09-06 16:41:58 +0300

    created login and register dto with thier rules

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,11 +1,8 @@
 import { IsEmail, IsString, Matches, MaxLength, MinLength } from "class-validator";
 
-export class ProjectOwnerRegiseterDto{
-    @IsString()
-    username: string;
-
+export class LoginDto {
     @IsEmail({}, { message: "follow this formating email@email.com" })
-    email:string;
+    email: string;
 
     @IsString()
     @MinLength(8, { message: 'Password length should be longer than 8' })
