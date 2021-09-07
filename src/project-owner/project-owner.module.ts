6d987a89af371134b750711caf9ff30d024fdedb
amer-bit <ny.1995@outlook.com>
2021-09-07 11:46:19 +0300

    imported auth module to be able to use guards

--- src/project-owner/project-owner.module.ts
+++ src/project-owner/project-owner.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProjectOwnerService } from './project-owner.service';
 import { ProjectOwnerController } from './project-owner.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports:[AuthModule],
   providers: [ProjectOwnerService],
   controllers: [ProjectOwnerController]
 })
