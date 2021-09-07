96290fe6e0e20fdd0e8776e7497fac27dbd30438
amer-bit <ny.1995@outlook.com>
2021-09-07 14:20:36 +0300

    created service for getting all projects and for changing project status

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { ProjectOwner } from 'src/auth/schema/project-owner.schema';
+import { UserRepository } from 'src/auth/user.repository';
+import { ChangeFundStatusDto } from './dto/change-fund-status.dto';
 
 @Injectable()
-export class AdminService {}
+export class AdminService {
+    constructor(private userRepo: UserRepository){}
+
+    async viewFundRequest(): Promise<ProjectOwner[]>{
+        const projectOwners = await this.userRepo.getAllUsersProjectsAndUsername();
+        return projectOwners;
+    }
+
+    async changeFundStatus(changeFundStatusDto: ChangeFundStatusDto){
+        await this.userRepo.changeProjectStatus(changeFundStatusDto);
+    }
+}
