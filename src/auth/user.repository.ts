7c2357abcd34b652c5769a9f6cc41b48c1faf48d
amer-bit <ny.1995@outlook.com>
2021-09-07 14:21:38 +0300

    created repository function that deals with changing the project status

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,11 +1,13 @@
-import { ConflictException, Injectable, InternalServerErrorException } from "@nestjs/common";
+import { ConflictException, Injectable, InternalServerErrorException, NotFoundException } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import * as bcrypt from 'bcrypt';
 import { Model } from "mongoose";
+import { ChangeFundStatusDto } from "src/admin/dto/change-fund-status.dto";
 import { LoginDto } from "./dto/login.dto";
 import { ProjectOwnerRegiseterDto } from "./dto/project-owner.register.dto";
 import { Admin } from "./schema/admin.schema";
 import { ProjectOwner } from "./schema/project-owner.schema";
+import { Project } from "./schema/project.schema";
 
 @Injectable()
 export class UserRepository {
@@ -40,7 +42,7 @@ export class UserRepository {
 
     async getAllUsersProjectsAndUsername(): Promise<ProjectOwner[]>{
         try {
-            const projectOwners = await this.ownerModel.find().select({username: 1, projects: 1})
+            const projectOwners = await this.ownerModel.find().select({ username: 1, projects: 1})
             return projectOwners;
         } catch (error) {
             throw new InternalServerErrorException;
@@ -57,6 +59,30 @@ export class UserRepository {
             }
         }
         return { email: null };
+    }
+
+    async changeProjectStatus(changeFundStatus: ChangeFundStatusDto){
+        const { userId, projectId, status } = changeFundStatus;
+        const projectOwner = await this.findProjectOwner(userId);
+        if(!projectOwner) throw new NotFoundException('Project owner could not be found');
+        for(let i = 0; i < projectOwner.projects.length; i++){
+            if(projectOwner.projects[i].id === projectId){
+                projectOwner.projects[i].status = status;
+                projectOwner.markModified('projects');                                
+                await this.saveModel(projectOwner);
+                return;
+            }
+        }        
+        throw new NotFoundException('Project could not be found');
+    }
+
+    private async findProjectOwner(userId: string): Promise<ProjectOwner>{
+        try {
+            const projectOwner = await this.ownerModel.findOne({_id: userId});
+            return projectOwner;
+        } catch (error) {
+            throw new InternalServerErrorException;
+        }
     }
 
     async saveModel(model: ProjectOwner | Admin):Promise<void>{
