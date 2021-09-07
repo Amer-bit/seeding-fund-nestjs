1b8f5978e0b858f9a0233649ebe0af16e14e769e
amer-bit <ny.1995@outlook.com>
2021-09-07 11:44:44 +0300

    created two routes one for getting signedin user projects and the other for creating projects fund request

--- src/project-owner/project-owner.controller.ts
+++ src/project-owner/project-owner.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { ProjectOwnerGuard } from 'src/auth/jwt.strategy';
+import { ProjectOwner } from 'src/auth/schema/project-owner.schema';
+import { CreateFundRequestDto } from './dto/create-fund-request.dto';
+import { ProjectOwnerService } from './project-owner.service';
 
-@Controller('project-owner')
-export class ProjectOwnerController {}
+@Controller('projectowner')
+@UseGuards(ProjectOwnerGuard)
+export class ProjectOwnerController {
+    constructor(
+        private ownerService: ProjectOwnerService,
+    ){}
+
+    @Get('/viewfundrequest')
+    async viewFundRequest(@Req() req){
+        const currentUser: ProjectOwner = req.user;
+        return currentUser.projects
+    }
+
+    @Post('/createfundrequest')
+    async createFundRequest(@Body() createFundRequestDto: CreateFundRequestDto, @Req() req){
+        const currentUser: ProjectOwner = req.user;
+        this.ownerService.createFundRequest(currentUser, createFundRequestDto);
+        return 'Project funding Request has been created';
+    }
+}
