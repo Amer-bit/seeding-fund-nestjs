a7e6a86ce340c1552085cfd8087f6ca8ed30ee16
amer-bit <ny.1995@outlook.com>
2021-09-07 14:18:45 +0300

    created routes for getting all projects and for changing project status

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Put, UseGuards } from '@nestjs/common';
+import { AdminGuard } from 'src/auth/admin-jwt.strategy';
+import { AdminService } from './admin.service';
+import { ChangeFundStatusDto } from './dto/change-fund-status.dto';
 
 @Controller('admin')
-export class AdminController {}
+@UseGuards(AdminGuard)
+export class AdminController {
+    constructor(private adminService: AdminService){}
+
+    @Get('/viewfundrequest')
+    async viewFundRequest(){
+        return this.adminService.viewFundRequest();
+    }
+
+    @Put('/changefundstatus')
+    async changeFundStatus(@Body() changeFundStatusDto: ChangeFundStatusDto){
+        return this.adminService.changeFundStatus(changeFundStatusDto);
+    }
+}
